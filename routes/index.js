35a8fa08df6747de80d9f53a68b663b04ae709c2
Karim Qalash <kekoessam.ke@gmail.com>
2023-06-24 00:41:40 +0300

    feat: :sparkles: create error handler middleware for routes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const AuthController = require('../controllers/auth.controller');
+const ErrorHandler = require('../middleware/error.middleware');
 
 // auth routes
-router.post('/register', AuthController.register);
-router.post('/login', AuthController.login);
+router.post('/register', ErrorHandler(AuthController.register));
+router.post('/login', ErrorHandler(AuthController.login));
 
 router.all('*',  (req, res) => res.status(400).json({ message: 'Bad Request.'}))
 

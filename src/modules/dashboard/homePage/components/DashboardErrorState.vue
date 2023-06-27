7676f960b66c061e149482d38df868e71a4ff599
Karim Qalash <kekoessam.ke@gmail.com>
2023-06-27 17:55:24 +0300

    feat(client): :sparkles: create dashboard error state ui

---
+++ src/modules/dashboard/homePage/components/DashboardErrorState.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-2/5 w-full flex flex-col justify-center items-center bg-white">
+    <font-awesome-icon
+      class="text-red-500"
+      :icon="faCircleExclamation"
+      size="3x"
+    />
+    <h1 class="text-2xl font-semibold text-gray-700">Something went wrong</h1>
+    <p class="text-gray-500">Please try again later</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+</script>

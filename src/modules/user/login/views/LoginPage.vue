7ec0d8c67995446ee7183dacf664ab42f6dd85ef
Karim Qalash <kekoessam.ke@gmail.com>
2023-06-27 17:37:13 +0300

    feat(client): :sparkles: add login page to its route

---
+++ src/modules/user/login/views/LoginPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <user-layout title="Sign in to your account">
+    <login-form />
+  </user-layout>
+</template>
+
+<script setup lang="ts">
+import UserLayout from "../../common/layout/UserLayout.vue";
+import LoginForm from "../components/LoginForm.vue";
+</script>
---
+++ src/router/app.resolver.ts
@@ -0,0 +1,4 @@
+export const LoginView = () =>
+  import(
+    /* webpackChunkName: "loginPage" */ "@/modules/user/login/views/LoginPage.vue"
+  );

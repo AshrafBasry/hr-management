d6ac86c1f82d4410d7a49defa99e998461083cf0
Karim Qalash <kekoessam.ke@gmail.com>
2023-06-27 17:48:57 +0300

    feat(client): :sparkles: create dashboard page layout

---
+++ src/modules/dashboard/common/components/DashboardLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <dashboard-navbar />
+    <div class="container card mx-auto px-4 sm:px-8">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DashboardNavbar from "./DashboardNavbar.vue";
+</script>

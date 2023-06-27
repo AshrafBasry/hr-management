fd25bf9842925a753386aa1bb446c046c696410e
Karim Qalash <kekoessam.ke@gmail.com>
2023-06-27 17:53:03 +0300

    feat(client): :sparkles: create dashboard add employee btn ui

---
+++ src/modules/dashboard/homePage/components/EmployeeAddBtn.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button
+      type="button"
+      class="create-employee__button bg-white text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded"
+      @click="openAddEmployeeModal"
+    >
+      <FontAwesomeIcon :icon="faPlus" class="w-5 h-5" />
+      <span class="ml-2">Add Employee</span>
+    </button>
+
+    <employee-details-modal
+      v-if="modalState.isOpened && modalState.isNewEmployee"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import EmployeeDetailsModal from "./EmployeeDetailsModal.vue";
+
+function openAddEmployeeModal() {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.create-employee {
+  &__button {
+    transition: all 0.2s ease-in-out;
+    padding: 0.5rem 1.5rem;
+    text-align: center;
+    display: inline-flex;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+}
+</style>

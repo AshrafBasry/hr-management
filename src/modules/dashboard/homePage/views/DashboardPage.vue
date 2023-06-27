179575cb9fd20734db7da6ff5e20fd883f0e003c
Karim Qalash <kekoessam.ke@gmail.com>
2023-06-27 17:47:30 +0300

    feat(client): :sparkles: create dashboard page layout

---
+++ src/modules/dashboard/homePage/views/DashboardPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <dashboard-layout>
+    <div class="dashboard">
+      <section>
+        <div
+          class="flex flex-col items-end px-6 py-8 mx-auto max-w-screen-lg h-screen"
+        >
+          <employee-add-btn />
+          <employee-filters />
+          <dashboard-loading-state v-if="dashboardState.isLoading" />
+          <dashboard-error-state v-if="dashboardState.hasError" />
+          <employee-table
+            v-if="!dashboardState.isLoading && !dashboardState.hasError"
+          />
+        </div>
+      </section>
+    </div>
+  </dashboard-layout>
+</template>
+
+<script setup lang="ts">
+import EmployeeAddBtn from "../components/EmployeeAddBtn.vue";
+import EmployeeFilters from "../components/EmployeeFilters.vue";
+import DashboardErrorState from "../components/DashboardErrorState.vue";
+import DashboardLoadingState from "../components/DashboardLoadingState.vue";
+import EmployeeTable from "../components/EmployeeTable/EmployeeTable.vue";
+import DashboardLayout from "../../common/components/DashboardLayout.vue";
+</script>
--- src/router/app.resolver.ts
+++ src/router/app.resolver.ts
@@ -2,3 +2,8 @@ export const LoginView = () =>
   import(
     /* webpackChunkName: "loginPage" */ "@/modules/user/login/views/LoginPage.vue"
   );
+
+export const DashboardView = () =>
+  import(
+    /* webpackChunkName: "dashboardPage" */ "@/modules/dashboard/homePage/views/DashboardPage.vue"
+  );
